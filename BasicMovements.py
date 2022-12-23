64f6a3aa3da5c4b56a4910b03e88f17dcc660a8e
Antonio Garofalo <avorio.dev@gmail.com>
2022-12-23 17:06:21 +0100

    Create BasicMovements.py

---
+++ BasicMovements.py
@@ -0,0 +1,14 @@
+from MyTello import MyTello
+
+tello = MyTello()
+
+
+# tello.start_streaming()
+# tello.takeoff()
+
+# tello.send_rc_control(0, 0, 100, 0, 2)
+# tello.send_rc_control(0, 0, 0, 0, 2)
+# tello.send_rc_control(0, 0, -100, 0, 2)
+
+# tello.land()
+# tello.stop_streaming()
