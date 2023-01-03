2c922ac5e9df001028228e553e3b76fa6217770b
Antonio Garofalo <avorio.dev@gmail.com>
2023-01-03 23:06:10 +0100

    Update CustomLogger.py

--- CustomLogger.py
+++ CustomLogger.py
@@ -1,16 +1,22 @@
 import logging
+import sys
+from logging.handlers import TimedRotatingFileHandler
 
 
 class CustomLogger(logging.Formatter):
+    ####################################################################################################################
+    # GLOBALS
 
+    # ---> Constants
     LOG_COL_GREY = "\x1b[37;20m"
     LOG_COL_CYANO = "\x1b[36;20m"
     LOG_COL_YELLOW = "\x1b[33;20m"
     LOG_COL_RED = "\x1b[31;20m"
     LOG_COL_BOLD_RED = "\x1b[31;1m"
     LOG_COL_RESET = "\x1b[0m"
-    # format = "%(asctime)s - %(name)s - %(levelname)s - %(message)s (%(filename)s:%(lineno)d)"
-    format = "%(name)s - %(asctime)s - [%(levelname)s] --> %(message)s"
+
+    # ---> Attributes
+    format = "%(asctime)s - [%(name)s] - %(levelname)s - %(message)s (%(filename)s:%(lineno)d)"
 
     FORMATS = {
         logging.DEBUG: LOG_COL_GREY + format + LOG_COL_RESET,
@@ -20,19 +26,33 @@ class CustomLogger(logging.Formatter):
         logging.CRITICAL: LOG_COL_BOLD_RED + format + LOG_COL_RESET
     }
 
-    def __init__(self, log_name: str):
-        super().__init__()
+    ####################################################################################################################
+    # CORE
 
-        self.logger = logging.getLogger(log_name)
-        self.logger.setLevel(logging.DEBUG)
-
-        # create console handler with a higher log level
-        console_handler = logging.StreamHandler()
-        console_handler.setLevel(logging.DEBUG)
+    # ---> Functions
+    def get_console_handler(self):
+        console_handler = logging.StreamHandler(sys.stdout)
         console_handler.setFormatter(self)
-        self.logger.addHandler(console_handler)
+        return console_handler
+
+    def get_file_handler(self):
+        file_handler = TimedRotatingFileHandler(self.log_name, when='midnight')
+        file_handler.setFormatter(self)
+        return file_handler
 
     def format(self, record):
         log_fmt = self.FORMATS.get(record.levelno)
         formatter = logging.Formatter(log_fmt)
         return formatter.format(record)
+
+    # ---> Constructor
+    def __init__(self, log_name: str, write_file: bool):
+        super().__init__()
+
+        self.log_name = log_name
+        self.logger = logging.getLogger(log_name)
+        self.logger.setLevel(logging.DEBUG)
+
+        self.logger.addHandler(self.get_console_handler())
+        if write_file:
+            self.logger.addHandler(self.get_file_handler())
