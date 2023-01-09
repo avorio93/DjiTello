d43da7d6004f3eec6d022c412a107df75cbfebe3
Antonio Garofalo <avorio.dev@gmail.com>
2023-01-09 07:18:10 +0100

    Update TelloPlus.py

--- TelloPlus.py
+++ TelloPlus.py
@@ -1,6 +1,10 @@
-from djitellopy import tello
+####################################################################################################################
+# IMPORTS
+import os
+import time
+
+from djitellopy import tello, BackgroundFrameRead
 from time import sleep
-from multipledispatch import dispatch
 from CustomLogger import CustomLogger
 from win32api import GetSystemMetrics
 import cv2  # If not working, add the path manually to the interpreter
@@ -19,22 +23,56 @@ class TelloPlus(tello.Tello):
 
     # ---> Constants
     CRITICAL_BATTERY = 15
-    LOG_NAME = 'TELLO_EXT'
+    WARNING_BATTERY = 35
+    LOG_NAME = "TELLO_PLUS"
 
     # ---> Attributes
 
     ####################################################################################################################
     # CORE
 
+    # ---> Constructor
+    def __init__(self):
+
+        # Logger init
+        self.logger = CustomLogger(self.LOG_NAME, write_file=False).logger
+
+        # Create a Tello instance and connect it to the drone
+        self.logger.info("Initialization...")
+        super().__init__()
+
+        try:
+            self.logger.info("Connecting to Tello...")
+            self.connect()
+        except Exception:
+            self.logger.critical("Connection refused")
+            raise Exception()
+
+        if self.get_battery() > 0:
+            self.logger.info("Successfully connected!")
+        else:
+            self.logger.error("Connection refused")
+            raise Exception()
+
+        # Check if drone is ready to Take-Off
+        self.logger.info("Battery Level:" + str(self.get_battery()))
+        if self.get_battery() >= TelloPlus.CRITICAL_BATTERY:
+            if self.get_battery() <= TelloPlus.WARNING_BATTERY:
+                self.logger.warning("Low Battery Level")
+            self.logger.info("Ready to fly!")
+
+        else:
+            self.logger.error("Battery level too low: unable to Take-Off")
+            raise Exception()
+
     # ---> Functions
-    @dispatch(tello.Tello, int, int, int, int, float)
+    # @dispatch(tello.Tello, int, int, int, int, float)
     def send_rc_control(self,
                         left_right_velocity: int,
                         forward_backward_velocity: int,
                         up_down_velocity: int,
                         yaw_velocity: int,
                         sleep_time: float):
-
         """
             Flying movement with the addiction of a time interval
         """
@@ -46,7 +84,7 @@ class TelloPlus(tello.Tello):
         if sleep_time > 0:
             sleep(sleep_time)
 
-        self.logger.debug('Movement ->',
+        self.logger.debug("Movement ->",
                           f'left_right:{left_right_velocity}',
                           f'forward_backward:{forward_backward_velocity}',
                           f'yaw:{yaw_velocity}',
@@ -55,7 +93,7 @@ class TelloPlus(tello.Tello):
 
     def takeoff(self):
         """
-            Start take-off with the addiction of a waiting interval of 2 secs.
+            Start Take-Off with the addiction of a waiting interval of 2 secs.
             In this way, the drone will takes-off avoiding unwanted movements
         """
 
@@ -64,7 +102,7 @@ class TelloPlus(tello.Tello):
         super().takeoff()
         self.send_rc_control(0, 0, 0, 0, 2)
 
-        self.logger.info('Take-Off completed')
+        self.logger.info("Take-Off completed")
 
     def land(self):
         """
@@ -95,10 +133,12 @@ class TelloPlus(tello.Tello):
 
     def get_img(self, resize_x: int, resize_y: int):
         """
-            Returns the frame captured by tello resized
-        """
+            Returns the frame captured by tello.
+            If resize params are given, the image will be resized
+            for example 720 x 480.
+            If zero is passed, it will use the half of resolution of your screen
 
-        # TODO
+        """
         width = GetSystemMetrics(0) / 2
         height = GetSystemMetrics(1) / 2
 
@@ -110,32 +150,20 @@ class TelloPlus(tello.Tello):
 
         img = self.get_frame_read().frame
         img = cv2.resize(img, (width, height))
-        # cv2.imshow("Tello Image", img)
+        cv2.imshow("Tello Image", img)
         cv2.waitKey(1)
+
         return img
 
-    # ---> Constructor
-    def __init__(self):
+    def save_img(self, img: BackgroundFrameRead):
+        img_name = f'\\{time.time()}.jpg'
+        out_path = os.path.join(os.path.join(os.environ['USERPROFILE']), 'Desktop')
+        out_path = out_path + '\\TelloCaptures'
+        if not os.path.isdir(out_path):
+            os.makedirs(out_path)
 
-        # Logger init
-        self.logger = CustomLogger(self.LOG_NAME, write_file=False).logger
-        self.logger.info('Connection init...')
+        out_path = out_path + img_name
 
-        # Create a Tello instance and connect it to the drone
-        super().__init__()
-        self.connect()
-
-        if self.get_battery() > 0:
-            self.logger.info('Successfully connected!')
-        else:
-            self.logger.error('Connection refused')
-            raise Exception()
-
-        # Check if drone is ready to take-off
-        self.logger.info('Battery Level:' + str(self.get_battery()))
-        if self.get_battery() >= TelloPlus.CRITICAL_BATTERY:
-            self.logger.info('Ready to fly!')
-
-        else:
-            self.logger.critical('Low battery level: unable to take off')
-            raise Exception()
+        cv2.imwrite(out_path, img.frame)
+        time.sleep(0.3)
+        self.logger.info("frame captured:", out_path)
